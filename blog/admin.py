c6bf9f87c68d6815d7c6ff493ec3527fdaf23420
eandachew <eandachew@gmail.com>
2025-12-26 12:30:12 +0000

    Add Comment model for post discussions

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -9,3 +9,4 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
 # Register your models here.
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -23,3 +23,28 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comment_author"
+    )
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
