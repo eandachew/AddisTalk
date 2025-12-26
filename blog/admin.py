7d821f2b6199591097dc5e5afbd8ee139925ce09
eandachew <eandachew@gmail.com>
2025-12-26 11:15:02 +0000

    adds a post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
 
 # Register your models here.
