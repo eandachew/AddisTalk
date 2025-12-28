6b8057247fb2f3f3afcdec14f5cb917ba080b6a6
eandachew <eandachew@gmail.com>
2025-12-28 19:53:26 +0000

    Add commenting feature to posts

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User # Import models to connect
 
-STATUS = (
-    (0, "Draft"),
-    (1, "Published")
-)
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Post(models.Model):
@@ -15,36 +12,29 @@ class Post(models.Model):
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ["-created_on"]
-
-    def __str__(self):
-        return self.title
     
-from django.db import models
-from django.contrib.auth.models import User
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post,
-        on_delete=models.CASCADE,
-        related_name="comments"
-    )
+        Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="comment_author"
-    )
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment by {self.author} on {self.post}"
+        return f"Comment {self.body} by {self.author}"
+        
