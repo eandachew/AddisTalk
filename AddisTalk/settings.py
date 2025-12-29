29da4fe128ff00037fcb1c086af96d7bf6063231
eandachew <eandachew@gmail.com>
2025-12-29 17:12:20 +0000

    fixing heroku

--- AddisTalk/settings.py
+++ AddisTalk/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#g^@mtwup^an=^4#6#qp$mc=+#707l*^64zoh&wrk_bbl3pdhr'
+SECRET_KEY = '63f4945d921d599f27ae4fdf5bada3f1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False   
