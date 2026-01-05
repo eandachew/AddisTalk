2c7e0f334c564cb85fcced344c507f2154cde1c4
Elias Kuma <eliaskuma@Eliass-MacBook-Pro.local>
2026-01-05 23:27:34 +0000

    comment added

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_fLhUtF8BTxj0@ep-rough-haze-agen0oah.c-2.eu-central-1.aws.neon.tech/silo_sling_droop_971937")
+os.environ.setdefault(
+    "SECRET_KEY", "django-insecure-91lk-8r&n^8np*ujmousev3tmvxa(mae)ntro!-&!and(ce!4e")
