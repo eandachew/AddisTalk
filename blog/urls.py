52da901f935c6a84cf002c57d525a908df8c2b99
Elias Kuma <eliaskuma@Eliass-MacBook-Pro.local>
2026-01-07 02:14:46 +0000

    add comment added and individual post working

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
-    
+    path('post/<slug:slug>/comment/', views.add_comment, name='add_comment'),
+
 ]
