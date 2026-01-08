ec9c798c8fc17a032deb62e1df52c43c3899397f
eandachew <eandachew@gmail.com>
2026-01-08 16:31:39 +0000

    fixing heroku 5

--- AddisTalk/settings.py
+++ AddisTalk/settings.py
@@ -167,7 +167,7 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Static files storage
-STATICFILES_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
