b2cf952a6a51a8a4295bd2b7d3500282baa95996
eandachew <eandachew@gmail.com>
2026-04-14 17:43:45 +0100

    database default sqlite added

--- AddisTalk/settings.py
+++ AddisTalk/settings.py
@@ -124,7 +124,7 @@ WSGI_APPLICATION = 'AddisTalk.wsgi.application'
 #}
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"))
 }
 if 'test' in sys.argv:
     DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
