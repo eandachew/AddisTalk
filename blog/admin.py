ecb72961055eb6ad19d9dd1bd976bd6cf5d24dda
eandachew <eandachew@gmail.com>
2026-04-20 12:56:46 +0100

    Enable Summernote toolbar in blog admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
 
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'author', 'status', 'created_on')
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ("content",)
+    summernote_fields = ('content',)
 
 
-# Register your models here.
-admin.site.register(Comment)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'approved', 'created_on')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('author__username', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
